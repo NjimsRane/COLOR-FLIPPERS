7d6edda60c8608f4a337ea17efbb9d38b075dfc0
NjimsRane <njims2011@gmail.com>
2022-10-17 11:18:06 +0100

    done with hex color page

--- COLOR-FLIPPER/App/JS/app.js
+++ COLOR-FLIPPER/App/JS/app.js
@@ -7,7 +7,8 @@ btn.addEventListener("click",function(){
     
     document.body.style.backgroundColor = colors[randomNumber];
     color.textContent = colors[randomNumber];
-    color.style.color = colors[randomNumber]
+    color.style.color = colors[randomNumber];
+    btn.style.color = colors[randomNumber];
 })
 
 function getRandomNumber(){
---
+++ COLOR-FLIPPER/App/JS/hex.js
@@ -0,0 +1,17 @@
+const hexColor = [1,2,4,5,6,7,8,9,"a","b","c","d","e","f"];
+const color = document.querySelector(".color");
+
+function colors (){
+    let hexValue ="#";
+    for(let i = 0 ; i < 6 ; i++){
+        hexValue += hexColor[getRandomColor()];
+    }
+    document.body.style.backgroundColor = hexValue;
+    color.style.color = hexValue;
+    color.textContent = hexValue;
+}
+setInterval(colors,2000)
+
+function getRandomColor(){
+    return Math.floor(Math.random()*hexColor.length)
+}
